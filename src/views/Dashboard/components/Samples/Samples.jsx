564ea210c0c58f2d06e5251b721a62cf92405b7f
Justin Green <justingreen@Justins-MacBook-Air.local>
2025-03-16 09:36:07 -0700

    cleaned up some mobile settings/padding

--- src/views/Dashboard/components/Samples/Samples.jsx
+++ src/views/Dashboard/components/Samples/Samples.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {sampleImg1a,sampleImg1b, sampleImg2a,sampleImg2b,sampleImg3a,sampleImg3b} from "../../../../const"
+import { IoCodeSlashOutline, IoEyeOutline } from "react-icons/io5";
 
 import "./Samples.css"
 
 const Samples = () => {
   const samples =[
-    {id:1,title:"Tech bookclub",image1:sampleImg1a,image2:sampleImg1b},
-    {id:2,title:"Finance Tracker",image1:sampleImg2a,image2:sampleImg2b},
-    {id:3,title:"Hangman",image1:sampleImg3a,image2:sampleImg3b},
+    {id:1,title:"Tech bookclub",image1:sampleImg1a,image2:sampleImg1b,link:"https://bookclub-fechallenge.vercel.app/",code:"https://github.com/jgreen721/bookclub-fechallenge"},
+    {id:2,title:"Learn Coding",image1:sampleImg2a,image2:sampleImg2b,link:"https://fe-quiz.vercel.app/",code:"https://github.com/jgreen721/fe_quiz"},
+    {id:3,title:"Finance Tracker",image1:sampleImg3a,image2:sampleImg3b,link:"https://personal-finance-firebase.vercel.app/",code:"https://github.com/jgreen721/personal-finance-firebase"},
   ]
   return (
     <div className="samples-section">
@@ -23,6 +24,14 @@ const Samples = () => {
           <div className="sample-title-div">
             <h3 className="sample-title">{sampleItem.title}</h3>
           </div>
+          <div className="sample-links-div">
+              <a className="sample-item-link" href={sampleItem.code} target="_blank">
+                <IoCodeSlashOutline/>
+              </a>
+              <a className="sample-item-link" href={sampleItem.link} target="_blank">
+                <IoEyeOutline/>
+              </a>
+           </div>
         </li>
       ))}
       </ul>
