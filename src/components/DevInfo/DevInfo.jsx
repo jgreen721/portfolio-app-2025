9d77a091435dfa2f6f96c34c69546d3c4145d038
Justin Green <justingreen@Justins-MacBook-Air.local>
2025-03-19 12:47:24 -0700

    save

--- src/components/DevInfo/DevInfo.jsx
+++ src/components/DevInfo/DevInfo.jsx
@@ -14,7 +14,7 @@ const DevInfo = ({handleAppTheme,appTheme}) => {
       <div className="desktop w-full">
         <MediaLinks/>
       </div>
-      <button onClick={()=>setShowContacts(showContacts=>showContacts = !showContacts)} className="btn dev-info-menu-btn">Show Contacts</button>
+      <button onClick={()=>setShowContacts(showContacts=>showContacts = !showContacts)} className="btn dev-info-menu-btn mobile-tablet">Show Contacts</button>
       <div className={`mobile-tablet mobile-contacts-menu-parent ${showContacts ? '' : 'hide-mobile-dev-menu'}`}>
         <div className={`menu-accordion mobile-contacts-menu`}>
           <Details/>
--- src/views/Dashboard/components/Contact/components/Form/Form.jsx
+++ src/views/Dashboard/components/Contact/components/Form/Form.jsx
@@ -1,5 +1,9 @@
 import React, {useRef, useState,useEffect} from 'react'
 import emailjs from '@emailjs/browser';
+import { IoMdCheckmark } from "react-icons/io";
+import { AiOutlineMail } from "react-icons/ai";
+import { IoCloseOutline } from "react-icons/io5";
+
 import { BsSend } from "react-icons/bs";
 
 import "./Form.css"
@@ -10,13 +14,18 @@ import "./Form.css"
 const Form = () => {
     const formRef = useRef();
     const progressRef = useRef();
+    const toasterContentRef = useRef();
+    const [showToast,setShowToast] = useState(false);
     const [clientError,setClientError] = useState(false);
     const [emailError,setEmailError] = useState(false);
     const [messageError,setMessageError] = useState(false);
 
     const handleFormSubmit=(e)=>{
         e.preventDefault();
-        // console.log("ENV_VARIABLES",import.meta.env.VITE_SERVICE_ID,import.meta.env.VITE_TEMPLATE_ID )
+        if(showToast){
+            //already processing email send
+            return;
+        }
 
         let formData = new FormData(formRef.current);
 
@@ -54,17 +63,20 @@ handleEmail();
     }
 
     const handleEmail=()=>{
+  
+
         emailjs
         .sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, formRef.current, {
           publicKey: import.meta.env.VITE_API_KEY,
         })
         .then(
           () => {
+            toasterContentRef.current.classList.remove("unblur-content");
+            setShowToast(true);
+
             console.log('SUCCESS!');
-            formRef.current["user_name"].value=""
-            formRef.current["user_email"].value=""
-            formRef.current["message"].value=""
-            growProgress();
+            setTimeout(()=>shrinkProgress(100),850);
+
           },
           (error) => {
             console.log('FAILED...', error);
@@ -74,28 +86,25 @@ handleEmail();
     }
 
 
-    const growProgress=()=>{
-        progressRef.current.style.width = `0px`
-        progressRef.current.classList.remove("celebrate-progress")
+    const shrinkProgress=(progressPercent)=>{
 
-       let progressInt;
-       let width = 0;
+    let progressInt;
+        progressInt = setInterval(()=>{
+           progressRef.current.style.transform = `scaleX(${progressPercent}%)`
+           progressPercent--; 
+           
+           if(progressPercent < 55 && !toasterContentRef.current.classList.contains("unblur-content")){
+               toasterContentRef.current.classList.add("unblur-content");
+           }
 
-       progressInt = setInterval(()=>{
-           width++;
-           console.log(`Width:${width}%`);
-       progressRef.current.style.width = `${width}%`
-
-           if(width >= 100){
+           if(progressPercent <= 0){
                clearInterval(progressInt)
-               setTimeout(()=>{
-                //    width = 0;
-                //    progressRef.current.style.width = `${width}%`
-                progressRef.current.classList.add("celebrate-progress")
-
-               },1250);
+               setShowToast(false);
+               formRef.current["user_name"].value=""
+               formRef.current["user_email"].value=""
+               formRef.current["message"].value=""
             }
-       },10)
+       },25);
     }
 
 
@@ -103,15 +112,30 @@ handleEmail();
   return (
     <div className="contact-form-section">
         <header>
-            <h2>Contact Form</h2>
+            <h2 onClick={()=>setShowToast(showToast=>showToast=!showToast)}>Contact Form</h2>
             <div className="h5-border-sign">
                 <h5 className="mid-thin">Lets bring <em>your</em> vision to life!</h5>
             </div>
-            <div className="contact-progress">
+            {/* <div className="contact-progress">
                 <div ref={progressRef} className="email-progress">
-                <h4>Successfully Sent</h4>
-
+                    <h4>Successfully Sent</h4>
                 </div>
+            </div> */}
+            <div className={`email-toaster ${showToast ? 'show-toaster' : 'hide-toaster'}`}>
+                <div ref={toasterContentRef} className="email-toaster-column">
+                    <div className="check-div">
+                        <IoMdCheckmark/>
+                    </div>
+                    <div>
+                        <h2 className="mb-1">S<span className="mid-thin">uccess</span></h2>
+                        <h4 className="mid-thin">Your email has been sent</h4>
+                    </div>
+                </div>
+                {/* <button onClick={()=>setShowToast(false)} className="close-btn">
+                    <IoCloseOutline/>
+                </button> */}
+                <div ref={progressRef} className="toaster-progress"></div>
+
             </div>
         </header>
         <form onSubmit={handleFormSubmit} className="contact-form" ref={formRef}>
